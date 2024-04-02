a18de1c159a889b9003726cfb10c8596e10e51ac
Brendan McGaw <Brendanmcgaw@gmail.com>
2024-04-02 22:38:46 +1100

    Basic homepage created with input search, functionality still needs to be developed.

--- src/Components/HomePage.tsx
+++ src/Components/HomePage.tsx
@@ -4,6 +4,9 @@ import { FaMagnifyingGlass } from "react-icons/fa6";
 export const HomePage = () => {
     return (
         <div className="homepageContainer">
+            <h3 className="homepageText">
+                Find your favourite movies, discover where to watch them.
+            </h3>
             <input
                 placeholder="Search for movies..."
                 className="searchInput"
