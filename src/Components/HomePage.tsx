3bc2d07cd35a73d9ff0e7027de4360a4bc3730d5
Brendan McGaw <Brendanmcgaw@gmail.com>
2024-04-02 22:40:44 +1100

    Homepage Comments added

--- src/Components/HomePage.tsx
+++ src/Components/HomePage.tsx
@@ -7,6 +7,8 @@ export const HomePage = () => {
             <h3 className="homepageText">
                 Find your favourite movies, discover where to watch them.
             </h3>
+
+            {/* TODO: functionality to go to Link of moviePage related to title of the movie, where the id matches an id that is present in the database. */}
             <input
                 placeholder="Search for movies..."
                 className="searchInput"
