06406e8d86a1969669377c51d0a0865016405c9a
Brendan McGaw <Brendanmcgaw@gmail.com>
2024-04-02 22:54:18 +1100

    Added a slogany thing.

--- src/Components/HomePage.tsx
+++ src/Components/HomePage.tsx
@@ -5,7 +5,8 @@ export const HomePage = () => {
     return (
         <div className="homepageContainer">
             <h3 className="homepageText">
-                Find your favourite movies, discover where to watch them.
+                Capturing the Heart of Cinema with Picture Pulse: Your Gateway
+                to Movie Magic!
             </h3>
 
             {/* TODO: functionality to go to Link of moviePage related to title of the movie, where the id matches an id that is present in the database. */}
