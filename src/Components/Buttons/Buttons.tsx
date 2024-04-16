b12ead9545ae55dab608000c197262ad0c05f585
Brendan McGaw <Brendanmcgaw@gmail.com>
2024-04-16 21:06:55 +1000

    Buttons got their own component folder

--- src/Components/Buttons/Buttons.tsx
+++ src/Components/Buttons/Buttons.tsx
@@ -3,3 +3,5 @@ import { StyledSubmitButton } from "./Buttons-styles";
 export const SubmitButton = () => {
     return <StyledSubmitButton type="submit">Submit</StyledSubmitButton>;
 };
+
+// TODO: Add the rest of the button components.
